e03d24f513a24db8a74cb7f95d035e2b745a21e4
mario.ambrosio <mario_conduta@hotmail.com>
2024-03-24 10:45:07 -0300

    Atualização no backend

--- backend/app.py
+++ backend/app.py
@@ -10,27 +10,25 @@ with open('data.json', 'r') as f:
 
 # --------------------------------------------------------------------------
 @app.route('/cidades', methods=['GET'])
-def listar_cidades():
-    cidades = list({viagem['city'] for viagem in data['transport']})
-    return jsonify({'cidades': cidades}), 200
+def list_cities():
+    city = list({trip['city'] for trip in data['transport']})
+    return jsonify({'cidades': city}), 200
 
 def convert_price_to_float(price_str):
     """Converte o preço de string para float."""
     return float(price_str.replace('R$', '').replace(',', '.'))
 
 # --------------------------------------------------------------------------
-@app.route('/viagens/<cidade>', methods=['GET'])
-def buscar_viagens(cidade):
-    viagens = [viagem for viagem in data['transport'] if viagem['city'].lower() == cidade.lower()]
-    if len(viagens) == 0:
-        return jsonify({'message': 'Nenhuma viagem encontrada para a cidade especificada'}), 404
+@app.route('/viagens/<city>', methods=['GET'])
+def find_trips(city):
+    trips = [trip for trip in data['transport'] if trip['city'].lower() == city.lower()]
     
-    viagem_mais_cara = max(viagens, key=lambda x: convert_price_to_float(x['price_confort']))
-    viagem_mais_barata = min(viagens, key=lambda x: convert_price_to_float(x['price_econ']))
+    most_expensive_trip = max(trips, key=lambda x: convert_price_to_float(x['price_confort']))
+    cheapest_trip = min(trips, key=lambda x: convert_price_to_float(x['price_econ']))
 
     return jsonify({
-        'viagem_mais_cara': viagem_mais_cara,
-        'viagem_mais_barata': viagem_mais_barata
+        'viagem_mais_cara': most_expensive_trip,
+        'viagem_mais_barata': cheapest_trip
     }), 200
     
 # --------------------------------------------------------------------------
